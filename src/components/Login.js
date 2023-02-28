ef2c792151fb609021399b49180bb906956b3e05
Pushkar Dangi <pushkardangi746@gmail.com>
2023-02-28 18:58:11 +0530

    added: login page

---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <main className="login">
+      <div className="loginTitle text-center title">Login</div>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div>
+          <button className="loginBtn">Sign In</button>
+        </div>
+
+        <p>
+          Don't have an account? <Link to="/register">Create one</Link>
+        </p>
+      </form>
+    </main>
+  );
+};
+
+export default Login;
