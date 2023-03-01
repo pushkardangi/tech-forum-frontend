a05553505693276086240e3985d32dee81467218
Pushkar Dangi <pushkardangi746@gmail.com>
2023-03-01 22:58:10 +0530

    added: register page

---
+++ src/components/Register.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import {useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, email, password });
+    setEmail("");
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <main className="register">
+      <h1 className="registerTitle">Create an account</h1>
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="registerBtn">Register</button>
+        <p>
+          Have an account? <Link to="/">Sign in</Link>
+        </p>
+      </form>
+    </main>
+  );
+};
+
+export default Register;
